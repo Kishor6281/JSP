a73b6f6ce23c6a5af0810ec5916372e6530019d1
Kishor6281 <93156764+Kishor6281@users.noreply.github.com>
2023-08-10 21:26:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+//Delete duplicates
+
+// let a = [1,2,3,4,56,1,3,]
+// let b = []
+
+// for(let i = 0 ;i<a.length ;i++) {
+//     let c =0 ;
+//     for(let j=i+1;j<a.length;j++) {
+//         if(a[i]==a[j]) {  
+//             c+=1 ;
+//         }
+//     }
+//     if(c==0) {
+//         b.push(a[i])
+//     }
+// }
+// console.log(b)
+
+
+// class mobile{
+//     constructor(n,p,s) {
+//         this.name = n ;
+//         this.price = p ;
+//         this.storage = s ;
+//     }
+// }
+
+// const res = new mobile("Jio",120000,'8gb') 
+
+// const res2 = new mobile("redMe",14000,"9gb")
+
+// const res3 = new mobile("Realme",22000,"23gb")
+// const res4 = new mobile("Vivo",21000,"20gb")
+// const res5 = new mobile("APple",20000,"4gb")
+
+// console.log(res)
+// console.log(res2)
+// console.log(res3)
+// console.log(res4)
+// console.log(res5)
+
+
+// const a =  [1,2,3,4] 
+
+// const res = a.map((element,index,array)=>{
+//     console.log(element,index,array)
+// })
+
+// console.log(res)
+
+
+
+// const a = [10,23,39,24] 
+
+// const r = a.filter((element)=>{
+//     return element>10 
+// })
+
+// console.log(r)
+
+
+// const a  = [1,3,5,2,1,6]
+
+// const res  = a.reduce((ele1,ele2)=>{
+//     return ele1 + ele2
+// })
+
+// console.log(res)
